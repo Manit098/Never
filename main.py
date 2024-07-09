8e25b9525ded60303b293814082193bab301a018
legendary <145838047+legendryPro@users.noreply.github.com>
2024-07-09 16:12:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ message_history = {}
 
 load_dotenv()
 
-GOOGLE_AI_KEY = os.getenv("GOOGLE_AI_KEY")
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-MAX_HISTORY = int(os.getenv("MAX_HISTORY"))
+GOOGLE_AI_KEY = "AIzaSyAb8_XlTKHPEFuTVph1JND_2RKizoY5Sqw"
+DISCORD_BOT_TOKEN = "MTIxMjY3OTE0NzUxNjYwMDMzMA.Gvj5qR.rYkUc2LzOjgB6gw4FhsFcuBg2Jc7n0Lb5Yc--Y"
+MAX_HISTORY = 15
 
 #---------------------------------------------System Prompt!-------------------------------------------------
 
